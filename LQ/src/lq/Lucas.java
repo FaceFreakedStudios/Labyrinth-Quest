1239946fee766393c7d3fed24b11f227a76e9672
GavinDes <gavin17.business@gmail.com>
2018-02-17 19:26:56 -0600

    Initial Commit

---
+++ LQ/src/lq/Lucas.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 gavin17.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package lq;
+
+/**
+ *
+ * @author gavin17
+ */
+public class Lucas
+{
+    private long hp;
+    private long ap;
+    private long xp;
+    
+    public void setStat(long hp, long ap)
+    {
+        this.hp = hp;
+        this.ap = ap;
+    }
+    
+    public long getHP()
+    {
+        return this.hp;
+    }
+    
+    public long getAP()
+    {
+        return this.hp;
+    }
+    
+    public long getXP()
+    {
+        return this.xp;
+    }
+}
