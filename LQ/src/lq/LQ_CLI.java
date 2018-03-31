d085984993782273144253a4accfb41e94cb30a4
GavinDes <gavin17.business@gmail.com>
2018-03-31 00:00:09 -0500

    Bare bone CLI planning

---
+++ LQ/src/lq/LQ_CLI.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 gavin17.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package lq;
+
+/**
+ *
+ * @author gavin17
+ */
+public class LQ_CLI
+{
+    char wall = '#';
+    char protag = '@';
+    char hole = 'o';
+    char enemy = '!';
+    char ground = '.';
+    char npc = '?';
+    char empty = ' ';
+    char water = '~'; // yet to be determined
+    char store = '^';
+    char trap = 'T'; // yet to be determined
+    // acid, fire, lava, animal???
+}
+
+
+// I don't know anything about graphics :/
