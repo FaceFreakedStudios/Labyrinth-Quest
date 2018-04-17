54cffda799c8f931e43c67f876cf67231e8ea67a
GavinDes <gavin17.business@gmail.com>
2018-04-16 20:16:13 -0500

    Organized map objects in a class, and added a fetchMap method which gets a .map file and converts it to a 2D char array.

---
+++ LQ/src/lq/LQCLI.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2018 gavin17.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lq;
+
+/**
+ *
+ * @author gavin17
+ */
+
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+
+class map_objects
+{
+    // Dynamic:
+    char protag = '@';
+    char enemy = '!';
+    char npc = '?';
+    // Static:
+    char wall = '#';
+    char hole = 'o';
+    char ground = '.';
+    char empty = ' ';
+    char water = '~';
+    char store = '$';
+    char building = '^';
+    char trap = ',';
+    char lava = '-';
+    char sign = '=';
+    char labyrinth = 'L';
+    char item = 'i';
+}
+
+public class LQCLI // Labyrinth Quest Command Line Interface
+{
+    static char[][] fetchMap(String map_name) throws IOException
+    {
+        File map_file = new File(map_name); // Map to load
+        Scanner map_scan = new Scanner(map_file);
+        char[][] map = new char[32][60]; // Map size limit
+        String map_row_temp = ""; 
+        int row_count = 0, column_count = 0;
+        while(map_scan.hasNextLine())
+        {
+            String line = map_scan.nextLine();
+            String[] map_row_str = line.split("!?^"); // Splits line by character
+            for(String map_square: map_row_str)
+            {
+                map_row_temp += map_square;
+            }
+            char[] map_row = map_row_temp.toCharArray(); // char array of row
+            for(char map_square: map_row) // Assigns map to char array
+            {
+                map[column_count][row_count] = map_square;
+                ++row_count;
+            }
+            ++column_count;
+            row_count = 0;
+            map_row_temp = "";
+        }
+        return map;
+    }
+}
