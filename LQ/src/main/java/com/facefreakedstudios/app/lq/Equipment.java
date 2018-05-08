2a118c5d5f3207c5cee9119bbfa334b35daf4efe
GavinDes <gavin17.business@gmail.com>
2018-05-07 20:05:59 -0500

    Added equipment class for all equipment in game (weapons, armor, rings).

---
+++ LQ/src/main/java/com/facefreakedstudios/app/lq/Equipment.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 gavin17.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facefreakedstudios.app.lq;
+
+/**
+ *
+ * @author gavin17
+ */
+public class Equipment
+{
+    public class equipment
+    {
+        protected long condition = 200; // Starting condition
+        
+        equipment(long starting_condition)
+        {
+            this.condition = starting_condition;
+        }
+        
+        public void setCondition(long condition)
+        {
+            if(broken())
+            {
+                this.condition = -999;
+            }
+            else
+            {
+                if(condition > 200)
+                {
+                    this.condition = 200; // equip condition doesn't exceed 200
+                }
+                else
+                {
+                    this.condition = condition;
+                }
+            }
+        }
+        
+        public boolean broken()
+        {
+            if(this.condition <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
